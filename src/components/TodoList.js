6ca8a2b80540b91c75b1068d5c3f8a2346bf2a7e
Baccon <bachcong.hn@gmail.com>
2025-04-10 11:13:58 +0700

    feat(FilterButton): added 4 filter buttons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleTodo, deleteTodo, selectFilteredTodos } from '../features/todosSlice';
-function TodoList(){
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  toggleTodo,
+  deleteTodo,
+  selectFilteredTodos,
+} from "../features/todosSlice";
+function TodoList() {
   const todos = useSelector(selectFilteredTodos);
   const dispatch = useDispatch();
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id} className={todo.completed ? 'completed' : ''}>
-          <span className='todo-text'>{todo.text}</span>
-          <input type='checkbox' checked={todo.completed} onChange={() => dispatch(toggleTodo(todo.id))}></input>
-          <button className='' onClick={() => dispatch(deleteTodo(todo.id))}>x</button>
+        <li key={todo.id} className={todo.completed ? "completed" : ""}>
+          <span className="todo-text">{todo.text}</span>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => dispatch(toggleTodo(todo.id))}
+          ></input>
+          <button className="" onClick={() => dispatch(deleteTodo(todo.id))}>
+            x
+          </button>
         </li>
       ))}
       {todos.length === 0 && <li>No todos available</li>}
     </ul>
   );
-};
+}
 
 export default TodoList;
